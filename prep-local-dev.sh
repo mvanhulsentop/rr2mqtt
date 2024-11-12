28df434cf8de40a188e7440fe6bc7887870aadeb
csowada <csowada@users.noreply.github.com>
2024-11-12 19:44:39 +0100

    Remove copy in this repo, use fork

--- prep-local-dev.sh
+++ prep-local-dev.sh
@@ -1,12 +1,13 @@
 # clone a specific tag of the iobRoker roborock adapter
-git clone -b 'v0.6.17' --single-branch --depth 1 https://github.com/copystring/ioBroker.roborock.git .
+# git clone -b 'v0.6.17' --single-branch --depth 1 https://github.com/csowada/ioBroker.roborock.git .
+git clone https://github.com/csowada/ioBroker.roborock.git .
 
 cp ./rr2mqtt-adapter.js ./ioBroker.roborock
 cp ./rr2mqtt-main.js ./ioBroker.roborock
 
 cd ./ioBroker.roborock
 
-npm npm remove @iobroker/adapter-core && npm ci --omit=dev && npm install jszip
+npm remove @iobroker/adapter-core && npm ci --omit=dev && npm install jszip
 
 #patch main.js file to use the modified adapter
 sed -i 's/require("@iobroker\/adapter-core")/require(".\/rr2mqtt-adapter")/g' main.js
